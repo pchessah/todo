6500b81f8ebe8d14817b29210b2b3c67650897e0
pchessah <paulchesa1@gmail.com>
2020-03-06 17:18:41 +0300

    done functionality

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form() {
   const [list, setList] = useState([]);
   const [listItem, setItemList] = useState("");
+  const [checkList, setCheckList] = useState([]);
 
   const handleChange = e => {
     setItemList(e.target.value);
@@ -16,18 +17,33 @@ function Form() {
 
   const handleDelete = ({ item }) => {
     const filteredArray = [...list];
-    const newList = filteredArray.filter(oneItem => {
-      return oneItem !== item;
+    const newList = filteredArray.filter(otherItems => {
+      return otherItems !== item;
     });
     setList(newList);
   };
-
-  const handleEdit = ({ item }, e) => {
-    let newListItem = window.prompt("Enter edited item");
-    setList([...list, newListItem]);
+  const handleEdit = ({ item }) => {
+    let newListItem = window.prompt("Enter new item");
     const filteredArray = [...list];
     filteredArray.splice(filteredArray.indexOf(item), 1, newListItem);
     setList(filteredArray);
+  };
+
+  const handleDone = ({ item }) => {
+    const filteredArray = [...list];
+    let doneList = filteredArray.filter(doneListItems => {
+      return doneListItems === item;
+    });
+    const newList = filteredArray.filter(otherItems => {
+      return otherItems !== item;
+    });
+    setCheckList([...checkList, ...doneList]);
+    setList(newList);
+  };
+
+  const handleReset = () => {
+    setCheckList([]);
+    setList([]);
   };
 
   return (
@@ -37,28 +53,81 @@ function Form() {
           What would you like to do?{"   "}
           <input type="text" value={listItem} onChange={handleChange} />
         </label>
-        <button type="submit">Enter</button>
+        <button type="submit" className="btn btn-primary">
+          Enter
+        </button>
+        <button onClick={handleReset}> Reset List </button>
       </form>
-      <div>
-        {list &&
-          list.map((item, index) => {
-            return (
-              <ul>
-                <li key={index} id={index}>
-                  {item} <br />
-                  <button onClick={() => handleEdit({ item })}>
-                    {" "}
-                    Edit{" "}
-                  </button>{" "}
-                  <button onClick={() => handleDelete({ item })}>
-                    {" "}
-                    Delete{" "}
-                  </button>
-                </li>
-              </ul>
-            );
-          })}
-      </div>
+      <h3>Tasks to be done</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Task</th>
+            <th>Edit/Delete</th>
+            <th>Done?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list &&
+            list.map((item, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{item}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-info"
+                      onClick={() => handleEdit({ item })}
+                    >
+                      Edit
+                    </button>
+
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => handleDelete({ item })}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-success"
+                      onClick={() => handleDone({ item })}
+                    >
+                      Done
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+      <br />
+      <hr />
+      <h2>Completed tasks</h2>
+      <table>
+        <thead>
+          <tr>
+            <th> # </th>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {checkList &&
+            checkList.map((itemChecked, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{itemChecked}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
